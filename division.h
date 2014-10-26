0338aaedcefb0b1288c604116e00eae76a876142
Lis0u <elise.schrav@gmail.com>
2014-10-26 12:37:39 +0100

    little change in put_pixel call

--- division.h
+++ division.h
@@ -5,10 +5,16 @@
 #include <stdio.h>
 #include <gtk-3.0/gtk/gtk.h>
 
-static void put_pixel (GdkPixbuf *pixbuf, int x, int y, guchar red, guchar green, guchar blue, guchar alpha);
+static void put_pixel (GdkPixbuf *pixbuf, int x, int y, 
+		       guchar red, guchar green, guchar blue);
 guchar* get_pixel(GdkPixbuf *pixbuf, int x, int y);
 void line (GdkPixbuf *pixbuf);
-int whiteline (GdkPixbuf *pixbuf,int y);
+int whiteline(GdkPixbuf *pixbuf,int y);
 void put_redline(GdkPixbuf *pixbuf, int y);
+void column(GdkPixbuf *pixbuf);
+int whitecolumn(GdkPixbuf *pixbuf,int y);
+void put_redcolumn(GdkPixbuf *pixbuf, int y);
+int isWhite(guchar *p);
+
 
 #endif
